1223884f681fbf8615227292089314fb51872008
mathiasdamh <mathias.damgaard.h@gmail.com>
2019-11-24 14:34:57 +0100

    workmaker færdig

---
+++ code/WorkMaker.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+  int i, char_amount;
+  char alphabet = 'a';
+  FILE *workload;
+
+  printf("How many characters?: ");
+  scanf(" %d", &char_amount);
+
+  workload = fopen("Workload.txt", "w");
+
+  for (i = 1; i <= char_amount; i++){
+    fputc(alphabet, workload);
+
+    ++alphabet;
+
+    if (alphabet == 'z' + 1){
+      alphabet = 'a';
+    }
+  }
+
+  return 0;
+}
