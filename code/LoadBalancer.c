21e7cc786709d6030fa5eef96df6370fd009199d
Karl <kholt19@student.aau.dk>
2019-11-25 12:26:02 +0100

    i don't know

--- code/LoadBalancer.c
+++ code/LoadBalancer.c
@@ -3,18 +3,19 @@
 #include <string.h>
 #include <time.h>
 
-#define BIG_ENOUGH 200
+#define BIG_ENOUGH 1000
 
 void generate_benchmarks(int *benchmarks, int n);
 void append_to_string(char *append_to, int number, int current_length);
 int seach_string(char *desired, int length, char *fstring, int *pos_point, int look_for);
 int not_number(char c);
+void restart_string(char *string, int n);
 
 int main(void){
-  int *benchmarks, num_nodes, counter_bench = 0, counter_node = 0, i, ch_this_line, pos_point;
-  char string_c[] = "workload000.txt", desired[BIG_ENOUGH], ch, fstring[19];
+  int *benchmarks, num_nodes, counter_bench = 0, counter_node = 0, i, ch_this_line, pos_point, ch_per_file;
+  char string_c[] = "workload000.txt", desired[BIG_ENOUGH], ch, fstring[20];
   FILE *fp_reader, *fp_writer;
-
+  
   srand(time(NULL));
   
   printf("Hey, how many nodes, would you like to do the work for you? ");
@@ -36,68 +37,110 @@ int main(void){
     exit(EXIT_FAILURE);
   }
 
+  /* This just tells the user the specification on each node. */
   for(i = 0; i < num_nodes; i++){
     printf("This is node nr. %2d it has the benchmark %d\n", i + 1, benchmarks[i]);
   }
 
+  /* This prints to the file temp, with standard '\[node nr.] [character]' and making sure the charcters per line doesn't exceed 15. */
   ch_this_line = 0;
   while((ch = getc(fp_reader)) != EOF){
-    ch_this_line++;
-    fprintf(fp_writer, "\\%d %c ", counter_node + 1, ch);
-    counter_bench++;
-    if(counter_bench == benchmarks[counter_node]){
-      counter_bench = 0;
-      counter_node = (counter_node + 1) % num_nodes;
-    }
-    if(ch_this_line > 15){
-      ch_this_line = 0;
-      fputc('\n', fp_writer);
+    if(ch != '\n'){
+      ch_this_line++;
+      fprintf(fp_writer, "\\%d %c ", counter_node + 1, ch);
+      counter_bench++;
+      /* makes it such that when a specific node has recived as many tasks as its bencmark it goes to the next node. */
+      if(counter_bench == benchmarks[counter_node]){
+        counter_bench = 0;
+        counter_node = (counter_node + 1) % num_nodes;
+      }
+      /* Makes sure the number of characters per line doesn't exceed 15. */
+      if(ch_this_line > 15){
+        ch_this_line = 0;
+        fputc('\n', fp_writer);
+      }
     }
   }
-  
+
+  /* closes the 2 files. */
   fclose(fp_reader);
   fclose(fp_writer);
 
+  /* opens 'temp.txt' in read mode. */
   fp_reader = fopen("temp.txt", "r");
   if(fp_reader == NULL){
     printf("Couldn't open temp.txt");
     exit(EXIT_FAILURE);
   }
-  
+
+  /* Runs once for each node. */
   for(i = 1; i <= num_nodes; ++i){
+    /* Sets fp_reader to be in the beginning of the file. */
     rewind(fp_reader);
+    /* Sets the num for character per file, to 0. mostly for debugging. */
+    ch_per_file = 0;
+    /* appends a number to the text file with the format 'workload[node nr.].txt'. */
     append_to_string(string_c, i, 8);
+    /* opens the file workload[node nr.]. */
     fp_writer = fopen(string_c, "w");
+    /* Checks the file as indead been opened. */
     if(fp_writer == NULL){
       printf("Couldn't open %s", string_c);
       exit(EXIT_FAILURE);
     }
-    append_to_string(string_c, i, 8);
+    /* goes aslong we haven't reached the end of the file */
     while((ch = getc(fp_reader)) != EOF){
       ungetc(ch, fp_reader);
+      /* set the string to pure '\0's. */
+      restart_string(desired, sizeof(desired)/sizeof(char));
       fgets(desired, BIG_ENOUGH, fp_reader);
-      /* printf("%s\n", desired); */
       pos_point = 0;
-      while(seach_string(desired, sizeof(desired)/sizeof(char), fstring, &pos_point, i) == 1)
-	fprintf(fp_writer, "%s", fstring);
+      /* While there still is more instances where the node is meansured, by '\[node nr]', keep running. */
+      while(seach_string(desired, sizeof(desired)/sizeof(char), fstring, &pos_point, i) == 1){
+        fprintf(fp_writer, "%s", fstring);
+        ch_per_file++;
+      }
     }
+    /* closes the file, which we are writing in, in the format 'workload[node nr.].txt'. */
     fclose(fp_writer);
   }
 
-  free(fp_reader);
+  /* Closes the reading file, 'temp.txt'. */
+  fclose(fp_reader);
 
+  /* I try to delete the file called temp.txt, but it doesn't seem to work. */
   if(remove("temp.txt") == 0)
-    printf("Deleted 'temp.txt' successfully.");
+    printf("Deleted 'temp.txt' successfully.\n");
   else
-    printf("Unable to delete 'temp.txt'.");
+    printf("Unable to delete 'temp.txt'.\n");
 
+  /* Start the nodes, such that they get their number and benchmark with. */
+  for(i = 0; i < num_nodes; i++){
+    restart_string(fstring, 20);
+    snprintf(fstring, 20, "START node.exe %d %d\n", i + 1, benchmarks[i]);
+    system(fstring);
+  }
+
+  /* frees the space which I've located in the heap. */
   free(benchmarks);
 
   return EXIT_SUCCESS;
 }
 
+/* Set a whole string to '\0'. */
+void restart_string(char *string, int n){
+  int i;
+  for(i = 0; i < n; i++){
+    string[i] = '\0';
+  }
+}
+
+/* Looks for a specific number in a string. */
 int seach_string(char *desired, int length, char *fstring, int *pos_point, int look_for){
   int i, j;
+  if(*pos_point > length)
+    return 0;
+  
   j = 0;
   for(i = *pos_point; i < length; i++){
     if(desired[i] == (char)(look_for + 48)){
@@ -106,12 +149,13 @@ int seach_string(char *desired, int length, char *fstring, int *pos_point, int l
 	if(desired[i] == ' ')
 	  i++;
 	else{
-	  fstring[j] = desired[i];
-	  j++;
+	  if(desired[i - 1] != '\\'){
+  	    fstring[j] = desired[i];
+            j++;
+	  }
 	  i++;
 	}
       }
-      i--;
       fstring[j] = '\0';
       *pos_point = i;
       return 1;
@@ -121,6 +165,7 @@ int seach_string(char *desired, int length, char *fstring, int *pos_point, int l
   return 0;
 }
 
+/* Checks that the charcter isn't a number. */
 int not_number(char c){
   int i;
   for(i = 0; i < 10; i++){
@@ -130,15 +175,15 @@ int not_number(char c){
   return 1;
 }
 
+/* replaces charcters in a string with a number, this is usfull in the format 'workload000.txt', where the 000 is replaced with the number. */
 void append_to_string(char *append_to, int number, int current_length){
   number = number;
   append_to[current_length]     = (char)(number / 100 + 48);
   append_to[current_length + 1] = (char)(number /  10 + 48);
   append_to[current_length + 2] = (char)(number       + 48);
-  
-  printf("%s\n", append_to);
 }
 
+/* This spits out random numbers between 1 and 5. */
 void generate_benchmarks(int *benchmarks, int n){
   int i;
   for(i = 0; i < n; ++i){
--- code/Node.c
+++ code/Node.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+<<<<<<< HEAD
+=======
+/* 
+>>>>>>> load_balancer
 #ifdef _WIN32
 #include <Windows.h>
 #else
@@ -6,6 +10,7 @@
 #endif
 
 int main(int argc, char* argv[]) {
+<<<<<<< HEAD
     /* expects at least 1 argument node number*/
     if (argc > 1) {
         /* open file */
@@ -19,3 +24,72 @@ int main(int argc, char* argv[]) {
         printf("not enough program arguments");
     }
 }
+=======
+    expects at least 1 argument node number
+    if (argc > 1) {
+         open file
+        part with benchmark??
+        
+        while not EOF
+        getc to get workload
+
+        sleep 1/benchmark
+    } else {
+        printf("not enough program arguments");
+    }
+}
+*/
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+void append_to_string(char *append_to, int number, int current_length);
+void wait_for(unsigned int secs);
+
+int main(int argc, char *argv[]){
+  int benchmark, node;
+  FILE *fp_reader, *fp_writer;
+  char ch, file_string_r[] = "workload000.txt", file_string_w[] = "tasks000.txt";
+
+  node      = (argv[1][0] - 48);
+  benchmark = (argv[2][0] - 48);
+  
+  printf("This is node: %c it got %d parameters and the benchmark %s\n", argv[1][0], argc, argv[2]);
+  
+  append_to_string(file_string_r, node, 8);
+  append_to_string(file_string_w, node, 5);
+  fp_reader = fopen(file_string_r, "r");
+  fp_writer = fopen(file_string_w, "w");
+  if(fp_reader == NULL || fp_writer == NULL){
+    printf("Couldn't open one of the files.");
+    exit(EXIT_FAILURE);
+  }
+
+  while((ch = getc(fp_reader)) != EOF){
+    wait_for((10 / benchmark));
+    printf("%c", ch);
+    fprintf(fp_writer, "%d ", (int)(ch));
+  }
+
+  fclose(fp_reader);
+  fclose(fp_writer);  
+
+  return 0;
+}
+
+/* from stackoverflow */
+void wait_for(unsigned int secs) {
+  unsigned int retTime = time(0) + secs;
+  while (time(0) < retTime);            
+}
+
+void append_to_string(char *append_to, int number, int current_length){
+  /*  printf("This is the number %d\n", number);*/
+  number = number;
+  append_to[current_length]     = (char)(number / 100 + 48);
+  append_to[current_length + 1] = (char)(number /  10 + 48);
+  append_to[current_length + 2] = (char)(number       + 48);
+  
+  /*  printf("%s\n", append_to); */
+}
+>>>>>>> load_balancer
