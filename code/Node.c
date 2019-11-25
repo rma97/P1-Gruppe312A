d4cb5b60c24a4e3a5f2d6356c600bf125a70676a
Karl <kholt19@student.aau.dk>
2019-11-25 12:18:36 +0100

    fix node

--- code/Node.c
+++ code/Node.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+/* 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+int main(int argc, char* argv[]) {
+    expects at least 1 argument node number
+    if (argc > 1) {
+         open file
+        part with benchmark??
+        
+        while not EOF
+        getc to get workload
+
+        sleep 1/benchmark
+    } else {
+        printf("not enough program arguments");
+    }
+}
+*/
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
